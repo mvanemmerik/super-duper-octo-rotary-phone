9610f307565209f146da9db972aed2f226400d62
Monty van Emmerik <mvanemmerik@me.com>
2018-06-01 05:45:28 -0400

    clean up

--- config/application.rb
+++ config/application.rb
@@ -15,12 +15,11 @@ module ToyApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-config.before_configuration do
-  env_file = File.join(Rails.root, 'config', 'local_env.yml')
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
-
+  config.before_configuration do
+    env_file = File.join(Rails.root, 'config', 'local_env.yml')
+    YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
